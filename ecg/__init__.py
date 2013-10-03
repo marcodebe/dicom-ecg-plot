758a3c2896c83275ee11e6bdf4b5aca4bb4f01f6
Marco De Benedetto <debe@galliera.it>
2013-10-03 08:49:56 +0200

    Added setup.py

---
+++ ecg/__init__.py
@@ -0,0 +1,3 @@
+from ecg import ECG
+
+__version__="1.0.0"
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+from ecg import __version__
+
+setup(name='dicomecg_convert',
+      version=__version__,
+      description='Dicom ECG Conversion',
+      long_description=open('README.md').read(),
+      author='Marco De Benedetto',
+      author_email='debe@galliera.it',
+      url='https://github.com/marcodebe/dicomecg_convert',
+      download_url='https://github.com/marcodebe/dicomecg_convert/downloads',
+      license='MIT',
+      packages=['ecg', ],
+      requires=['pydicom',
+                'numpy',
+                'matplotlib',
+                'cython',
+                'scipy',
+                'docopt',
+                'requests'],
+      dependency_links=['http://github.com/scipy/scipy/', ]
+      )
