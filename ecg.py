1d189b03a8144d70a2f2a77d6037140c9c38bd78
Marco De Benedetto <debe@galliera.it>
2013-10-03 07:59:31 +0200

    It should have been committed in 0a6b0e62d8

--- ecg.py
+++ ecg.py
@@ -1,31 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""ECG (waveform) Dicom module
 
-"""ECG Conversion Tool
-
-Usage:
-    ecgconvert.py <inputfile> [--layout=LAYOUT] [--usetex] [--output=FILE|--format=FMT]
-    ecgconvert.py <stu> <ser> <obj> [--layout=LAYOUT] [--usetex] [--output=FILE|--format=FMT]
-
-Options:
-    -h, --help                 This help.
-    <inputfile>                Input dicom file.
-    <stu> <ser> <obj>          studyUID seriesUID objectUID
-                               UIDs for WADO download.
-    -l LAYOUT --layout=LAYOUT  Layout [default: 3X4_1].
-    -o FILE --output=FILE      Output file (format deduced by extension).
-    -f FMT --format=FMT        Output format.
-    --usetex                   Use TeX for better fonts (25% slower).
-
-Valid layouts are: 3X4_1, 3X4, 12X1
-
-The output format is deduced from the extension of the filename.
-
-Supported formats: eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba,
-                   svg, svgz, tif, tiff.
+Read and plot images from DICOM ECG waveforms.
 """
 
-from docopt import docopt
 from datetime import datetime
 from matplotlib import pylab as plt
 from scipy.signal import butter, lfilter
@@ -233,8 +211,9 @@ class ECG(object):
             _save(output)
             return output.getvalue()
 
-    def plot(self, layout):
+    def plot(self, layoutid):
 
+        layout = LAYOUT[layoutid]
         rows = len(layout)
 
         for numrow, row in enumerate(layout):
@@ -273,51 +252,3 @@ class ECG(object):
 
         # A4 size in inches
         self.fig.set_size_inches(11.69, 8.27)
-
-
-def convert(source, layout, outformat=None, outputfile=None):
-
-    def err(msg):
-        raise(Exception(msg))
-
-    if isinstance(source, dict):
-        if len(source) == 3:
-            ecg = ECG(**source)
-        else:
-            err("source must be a dictionary of stu, ser and obj")
-    elif isinstance(source, basestring):
-        ecg = ECG(filename=source)
-    else:
-        err("`source´ must be a path/to/file.ext string\n" +
-            "or a dictionary of stu, ser and obj")
-
-    ecg.draw_grid()
-    ecg.print_info()
-    ecg.plot(layout)
-    return ecg.save(outformat=outformat, outputfile=outputfile)
-
-if __name__ == '__main__':
-
-    arguments = docopt(__doc__, version='ECG Convert 0.1')
-    inputfile = arguments['<inputfile>']
-    stu = arguments['<stu>']
-    ser = arguments['<ser>']
-    obj = arguments['<obj>']
-    outputfile = arguments['--output']
-    usetex = arguments['--usetex']
-    outformat = arguments['--format']
-    layout = LAYOUT[arguments['--layout']]
-
-    if usetex:
-        plt.rc('text', usetex=True)
-
-    if inputfile:
-        source = inputfile
-        output = convert(inputfile, layout, outformat, outputfile)
-    else:
-        source = {'stu': stu, 'ser': ser, 'obj': obj}
-
-    output = convert(source, layout, outformat, outputfile)
-
-    if output:
-        print output
