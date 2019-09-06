dd497ca4e9513591b0d033fe98cceb9dbd83c79a
Marco De Benedetto <debe@galliera.it>
2019-09-06 14:29:24 +0200

    Changes for PyPi and renamed the project
    This module has been uploaded on pypi.org with the new name 'dicom-ecg-plot'.
    
    It will be installable via pip:
    $ pip install dicom-ecg-plot
    
    The github project was also renamed 'dicom-ecg-plot'.

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
+import setuptools
 
-# Use setuptools if we can
-try:
-    from setuptools.core import setup
-except ImportError:
-    from distutils.core import setup
-from ecg import __version__
 
-setup(name='dicomecg_convert',
-      version=__version__,
-      description='Dicom ECG Conversion',
-      long_description=open('README.md').read(),
-      author='Marco De Benedetto',
-      author_email='debe@galliera.it',
-      url='https://github.com/marcodebe/dicomecg_convert',
-      download_url='https://github.com/marcodebe/dicomecg_convert/downloads',
-      license='MIT',
-      packages=['ecg', ],
-      )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("VERSION", "r") as fh:
+    version = fh.read().strip('\n')
+
+
+setuptools.setup(
+        name='dicom-ecg-plot',
+        version=version,
+        description='Plot Dicom ECG Waveforms',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author='Marco De Benedetto',
+        author_email='debe@galliera.it',
+        url='https://github.com/marcodebe/dicomecg_convert',
+        packages=setuptools.find_packages(),
+        scripts=['dicom-ecg-plot'],
+        install_requires=[
+            'pydicom',
+            'numpy',
+            'matplotlib',
+            'scipy',
+            'docopt',
+            'requests',
+            ],
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Topic :: Scientific/Engineering :: Medical Science Apps.',
+            'Intended Audience :: Healthcare Industry',
+            ],
+        )
